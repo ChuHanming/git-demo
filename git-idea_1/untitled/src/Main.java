4b2c06fbd80ce6b5225bbd304268127c6628eea2
ChuHanming <1055297160@qq.com>
2023-03-23 21:23:06 +0800

    yes

--- git-idea_1/untitled/src/Main.java
+++ git-idea_1/untitled/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("第一次提交");
+        System.out.println("成功了！");
     }
 }
